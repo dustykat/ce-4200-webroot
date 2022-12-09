803ac2618d0aae4f93ccc4181dc4ebc8bda0c32b
theodore cleveland <ted.cleveland@gmail.com>
2022-12-09 23:41:33 +0000

    intro jupyterbook

---
+++ ce-4200-notes/_build/jupyter_execute/lessons/introduction/introduction.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Introduction
+
+# In[ ]:
+
+
+
+
+
+# ## References
+# 
+# [Introduction Lecture *Lawson 2022*](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/LECTURE-SOURCE/01-LECTURE_1_CE4200IntroLecture-S2022.pdf)
+
+# In[ ]:
+
+
+
+
