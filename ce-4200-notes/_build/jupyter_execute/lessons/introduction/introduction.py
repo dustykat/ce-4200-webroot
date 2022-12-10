7b41af85c0a4bc99697b00d38e24f42a4930c2e5
theodore cleveland <ted.cleveland@gmail.com>
2022-12-10 00:02:21 +0000

    trust

--- ce-4200-notes/_build/jupyter_execute/lessons/introduction/introduction.py
+++ ce-4200-notes/_build/jupyter_execute/lessons/introduction/introduction.py
@@ -3,15 +3,37 @@
 
 # # Introduction
 
-# In[ ]:
-
-
-
-
+# ## Outcomes
+# - Outcomes are statements that describe what individuals are expected to know and be able to do by the time of entry into the practice of civil engineering at the professional level in the 21st century—that is, **attain licensure**.
+# - Outcomes define the knowledge, skills, and attitudes that individuals acquire through appropriate formal education and pre-licensure experience.
+# 
+# ## Civil Engineering Body Of Knowledge 3
+# 
+# ![](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/lessons/introduction/CEBOK3.png)
+# - Published Mar 2019
+# - Compliant with 2016-17 and later ABET Program Objectives
+# - 21 Learning Outcomes
+# - Cognitive and Affective levels of achievement
+# 
+# ## Example 1.2
+# CEBOK3 Professional Responsibilities
+# 1. Domain (cognitive or affective)
+# 2. Level of achievement (SIX levels, Bloom’s taxonomy)
+# 3. Demonstrated ability (note the verb)
+# 4. Typical pathway (expert status, for licensure, for undergraduate study)
+# 
+# ![](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/lessons/introduction/tableES1-annotate.png)
+# 
+# ![](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/lessons/introduction/table2-20a-annotate.png)
+# 
+# ![](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/lessons/introduction/table2-20b-annotate.png)
 
 # ## References
 # 
-# [Introduction Lecture *Lawson 2022*](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/LECTURE-SOURCE/01-LECTURE_1_CE4200IntroLecture-S2022.pdf)
+# 1. [Introduction Lecture *Lawson 2022*](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/LECTURE-SOURCE/01-LECTURE_1_CE4200IntroLecture-S2022.pdf)
+# 2. [CEBOK3 Instructor Fact Sheet](https://www.asce.org/-/media/asce-images-and-files/career-and-growth/cebok/documents/asce-cebok3-infographic-handout.pdf)
+# 3. [CEBOK3 User Factsheet](https://www.asce.org/-/media/asce-images-and-files/initiatives/documents/cebok-info-full.pdf)
+# 4. [CEBOK3 Full Document (PDF)](https://ascelibrary.org/doi/book/10.1061/9780784415221)
 
 # In[ ]:
 
