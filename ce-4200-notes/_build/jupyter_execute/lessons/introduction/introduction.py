d4b4444402c883d3ec33fa6d68c34ce33286f1bc
theodore cleveland <ted.cleveland@gmail.com>
2022-12-10 00:32:30 +0000

    introduction lesson

--- ce-4200-notes/_build/jupyter_execute/lessons/introduction/introduction.py
+++ ce-4200-notes/_build/jupyter_execute/lessons/introduction/introduction.py
@@ -15,6 +15,13 @@
 # - 21 Learning Outcomes
 # - Cognitive and Affective levels of achievement
 # 
+# ## CEBOK3 Outcomes
+# **Civil Engineering Body of Knowledge for the 21 st Century**
+# 
+# ![](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/lessons/introduction/tableES1-annotate.png)
+# 
+# <hr>
+# 
 # ## Example 1.2
 # CEBOK3 Professional Responsibilities
 # 1. Domain (cognitive or affective)
@@ -22,11 +29,40 @@
 # 3. Demonstrated ability (note the verb)
 # 4. Typical pathway (expert status, for licensure, for undergraduate study)
 # 
-# ![](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/lessons/introduction/tableES1-annotate.png)
+# <hr>
+# 
+# > Cognitive Domain. The cognitive domain (Bloom, 1956) involves knowledge and the development of intellectual skills. This includes the recall or recognition of specific facts, procedural patterns, and concepts that serve in the development of intellectual abilities and skills.
+# 
+# ![](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/lessons/introduction/table2-20b-annotate.png)
+# 
+# <hr>
+# 
+# > Affective Domain: includes “the manner in which we deal with things emotionally, such as feelings, values, appreciation, enthusiasms, motivations, and attitudes” (Krathwohl, Bloom, Masia, 1973.) Excitement, challenge, frustration, and even trauma are all emotions and experiences that require us to dip into our bank of coping skills and apply them in order to successfully navigate interpersonal situations.
 # 
 # ![](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/lessons/introduction/table2-20a-annotate.png)
 # 
-# ![](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/lessons/introduction/table2-20b-annotate.png)
+# <hr>
+# 
+# > Psychomotor Domain: includes physical movement, coordination, and use of the motor-skill areas. Development of these skills requires practice and is measured in terms of speed, precision, distance, procedures, or techniques in execution.  
+# 
+# **How does the psychomotor domain fit in civil engineering?**
+# 
+# ## Where this course fits in:
+# 
+# {Image CE Dynamic Enrollment Management Plan}
+# 
+# {Complete Comparison Matrix}
+# 
+# ## Syllabus
+# 
+# ## Schedule
+# 
+# ## Textbooks
+# 
+# 1. [CEBOK (Free)](https://ascelibrary.org/doi/book/10.1061/9780784415221)
+# 2. [NCEES Supplied Reference (Free)](https://help.ncees.org/article/87-ncees-exam-reference-handbooks)
+# 3. [PPI FE Civil Review (~\$29.99)](https://a2ztextbook.com/products/ppi-fe-civil-practice-comprehensive-practice-for-the-ncees-fe-civil-exam-first-edition?variant=41620050903226&currency=USD&utm_medium=product_sync&utm_source=google&utm_content=sag_organic&utm_campaign=sag_organic) Full price is \$160-ish, don't pay full price, you should be able to find one at the reduced price
+# 4. [PPI Online Training Exams (~\$38)](https://app.ppehq.com/learn/FE-Civil-Exam-2022) You need to complete an entire exam package by the last day of class. Submit a screen capture of the completion diagnostic score to recieve credit for this portion of the class.
 
 # ## References
 # 
@@ -34,6 +70,7 @@
 # 2. [CEBOK3 Instructor Fact Sheet](https://www.asce.org/-/media/asce-images-and-files/career-and-growth/cebok/documents/asce-cebok3-infographic-handout.pdf)
 # 3. [CEBOK3 User Factsheet](https://www.asce.org/-/media/asce-images-and-files/initiatives/documents/cebok-info-full.pdf)
 # 4. [CEBOK3 Full Document (PDF)](https://ascelibrary.org/doi/book/10.1061/9780784415221)
+# 5. [Bloom's Learning Taxonomy](https://thepeakperformancecenter.com/educational-learning/learning/process/domains-of-learning/affective-domain/)
 
 # In[ ]:
 
