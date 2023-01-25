d6f11aa95319d9422f7b1a6399e090443cdc5541
theodore cleveland <ted.cleveland@gmail.com>
2023-01-25 17:50:13 +0000

    trusty

--- ce-4200-notes/_build/jupyter_execute/lessons/trusty/trusty.py
+++ ce-4200-notes/_build/jupyter_execute/lessons/trusty/trusty.py
@@ -5,7 +5,7 @@
 # 
 # ** Transfer notes below into Jupyter{Book} format and formal typeset.**
 # 
-# Review notes by Dr. Lawson at [Trust and Trustworthiness](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/LECTURE-SOURCE/LECTURE 2_OK-GEOINST_PRES_Trust&Trustworthiness.pdf)
+# Review notes by Dr. Lawson at [Trust and Trustworthiness (.pdf notes)](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/LECTURE-SOURCE/LECTURE_2_OK-GEOINST_PRES_Trust&Trustworthiness.pdf)
 # ## Trust Model(s)
 # Trust in engineering - why does it matter?
 # 
@@ -13,7 +13,7 @@
 # 
 # How does this relate to professional engineering?
 # 
-# [Zero-Trust Model (mighty internet](https://www.akamai.com/our-thinking/zero-trust/zero-trust-security-model?gclid=EAIaIQobChMIl9bQhKHj_AIVFBTUAR1FwQjNEAAYBCAAEgL6qPD_BwE&utm_source=google&utm_medium=cpc&utm_campaign=F-MC-57661&utm_term=zero%20trust&utm_content=US&ef_id=EAIaIQobChMIl9bQhKHj_AIVFBTUAR1FwQjNEAAYBCAAEgL6qPD_BwE:G:s&s_kwcid=AL!5241!3!587547811179!e!!g!!zero%20trust%20model!16563846121!131400992781)
+# [Zero-Trust Model (browser search of the mighty internet)](https://www.akamai.com/our-thinking/zero-trust/zero-trust-security-model?gclid=EAIaIQobChMIl9bQhKHj_AIVFBTUAR1FwQjNEAAYBCAAEgL6qPD_BwE&utm_source=google&utm_medium=cpc&utm_campaign=F-MC-57661&utm_term=zero%20trust&utm_content=US&ef_id=EAIaIQobChMIl9bQhKHj_AIVFBTUAR1FwQjNEAAYBCAAEgL6qPD_BwE:G:s&s_kwcid=AL!5241!3!587547811179!e!!g!!zero%20trust%20model!16563846121!131400992781)
 # 
 # [Zero-Trust Model (IBM)](https://www.ibm.com/topics/zero-trust)
 # 
@@ -21,7 +21,8 @@
 # 
 # ## References
 # 
-# 1. [Handout Trust and Trustworthiness](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/LECTURE-SOURCE/LECTURE 2_OK-GEOINST_PRES_Trust&Trustworthiness-handout.pdf)
+# 1. [Handout Trust and Trustworthiness](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/LECTURE-SOURCE/LECTURE_2_OK-GEOINST_PRES_Trust&Trustworthiness-handout.pdf)
+# 2. [Trust and Trustworthiness (pptx) source file](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/LECTURE-SOURCE/LECTURE_2_OK-GEOINST_PRES_Trust&Trustworthiness.pptx)
 
 # In[ ]:
 
