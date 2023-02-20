2837831867caa31cdc666ec29e2ffd2958e99b96
theodore cleveland <ted.cleveland@gmail.com>
2023-02-20 17:37:29 +0000

    initial licensing

---
+++ ce-4200-notes/_build/jupyter_execute/lessons/licensing/licensing.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Engineering Licensing
+# 
+# **Under development** 
+# 
+# ## Practice of Engineering
+# 
+# The practice of engineering means the performance of or an offer or attempt to perform any public or private service or creative work, the adequate performance of which requires **engineering education**, training, and **experience** in applying special knowledge or *judgment* of the mathematical, physical, or engineering sciences to that service or creative work.
+# 
+# The practice of engineering includes:
+# 
+# 1. consultation, investigation, evaluation, analysis, planning, engineering for program management, providing an expert engineering opinion or testimony, engineering for testing or evaluating materials for construction or other engineering use, and mapping;
+# 2. design, conceptual design, or conceptual design coordination of engineering works or systems;
+# 3. development or optimization of plans and specifications for engineering works or systems;
+# 4. planning the use or alteration of land or water or the design or analysis of works or systems for the use or alteration of land or water;
+# 5. responsible charge of engineering teaching or the teaching of engineering;
+# 6. performing an engineering survey or study;
+# 7. engineering for construction, alteration, or repair of real property;
+# 8. engineering for preparation of an operating or maintenance manual;
+# 9. engineering for review of the construction or installation of engineered works to monitor compliance with drawings or specifications;
+# 10. a service, design, analysis, or other work performed for a public or private entity in connection with a utility, structure, building, machine, equipment, process,system, work, project, or industrial or consumer product or equipment of a mechanical, electrical, electronic, chemical, hydraulic, pneumatic, geotechnical, or thermal nature; or
+# 11. providing an engineering opinion or analysis related to a certificate of merit under Chapter 150, Civil Practice and Remedies Code; or
+# 12. any other professional service necessary for the planning, progress, or completion of an engineering service
+# 
+# 
+# ## Texas Engineering Practice Act
+# 
+# The TEPA is the legislative construct that oversees engineering and establishes what it is, is not, who can participate, and issues licenses through the Texas Board of Professional Engineers.
+# 
+# [Texas Engineering Practice Act (go to pg 2)](https://engineers.texas.gov/downloads/enf_pub.pdf)
+# 
+# 
+# ## Education
+# 
+# Graduate of ABET accredited program:
+# 
+# ![](graduateengineer.png)
+# 
+# [Texas Engineering Practice Act (go to pg 10)](https://engineers.texas.gov/downloads/enf_pub.pdf)
+# 
+# 
+# 
+# ## FE/EIT 
+# 
+# ![](EIT.png)
+# 
+# [Texas Engineering Practice Act (go to pg 15)](https://engineers.texas.gov/downloads/enf_pub.pdf)
+# 
+# 
+# 
+# ## PE Exam
+# 
+# [Basic Requirements](https://pels.texas.gov/lic_basic.htm)
+# 
+# No specific reference in TEPA, but by reference to Board. [TBPE](https://pels.texas.gov/)
+# 
+# ## SER
+# 
+# Supplimentary experience record, a requited part of the application.
+# 
+# [Example SER](https://pels.texas.gov/downloads/serinfo.pdf)
+# 
+# 
+
+# ## References
+# 
+# 1. [Lawson (2022), *Getting Licensed* Handout to accompany CE 4200, Whitacre College of Engineering, Texas Tech University](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/lessons/licensing/LECTURE5A_GettingLicensed_CE4200_S2022-handout.pdf)
+# 
+# 2. [Lawson (2022), *Engineering License Application* Handout to accompany CE 4200, Whitacre College of Engineering, Texas Tech University](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/lessons/licensing/LECTURE5B_EngineeringLicensureApplication_CE4200_S2022-handout.pdf)
+# 
+# 3. [Lawson (2022), *FE Exam* Handout to accompany CE 4200, Whitacre College of Engineering, Texas Tech University](http://54.243.252.9/ce-4200-webroot/ce-4200-webroot/ce-4200-notes/lessons/licensing/LECTURE6_CE4200FEEXAM_S2022-handout.pdf)
+# 
+# 3. [Environmental Engineering BOK](https://www.aaees.org/assets/docs/publications/EEBodyofKnowledge.pdf)
+# 
+# 4. [ASCE BOK-2](https://ascelibrary.org/doi/book/10.1061/9780784409657) You will need this for the homework; although BOK-3 may be sufficient.
+# 
+# 5. [Texas Engineering Practice Act](https://engineers.texas.gov/downloads/enf_pub.pdf)
+
+# In[ ]:
+
+
+
+
