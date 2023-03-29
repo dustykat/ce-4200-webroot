73394532ac2fa189ca454559ba86cce40ff9c8fe
theodore cleveland <ted.cleveland@gmail.com>
2023-03-29 16:25:22 +0000

    add exercises

---
+++ ce-4200-notes/_build/jupyter_execute/lessons/leadershit/leadershit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Engineering Leadership
+# 
+# **Under development** 
+# 
+# Use link below to older lecture notes
+# 
+
+# ## References
+# 
+# 1. [Lawson (2021), *Engineering Leadership* Lecture to accompany CE 4200, Whitacre College of Engineering, Texas Tech University](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/LECTURE-SOURCE/LECTURE_12_CE_4200_CIVIL_ENGINEERING_LEADERSHIP_S2022.pdf)
+# 
+
+# In[ ]:
+
+
+
+
