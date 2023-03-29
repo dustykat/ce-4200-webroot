5f5d15fe4f23eb19cf92ee346feaa43a7df48127
theodore cleveland <ted.cleveland@gmail.com>
2023-03-29 16:32:22 +0000

    add qbs

--- ce-4200-notes/_build/jupyter_execute/lessons/qbs/qbs.py
+++ ce-4200-notes/_build/jupyter_execute/lessons/qbs/qbs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Diversity and Inclusion
+# # Qualification Based Selection
 # 
 # **Under development** 
 # 
@@ -10,7 +10,7 @@
 
 # ## References
 # 
-# 1. [Lawson (2021), *Diversity and Inclusion* Lecture to accompany CE 4200, Whitacre College of Engineering, Texas Tech University](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/LECTURE-SOURCE/LECTURE13_DIVERSITY_INCLUSION_F2021.pdf)
+# 1. [Lawson (2021), *Qualification Based Selection* Lecture to accompany CE 4200, Whitacre College of Engineering, Texas Tech University](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/LECTURE-SOURCE/LECTURE9_CE4200QBS_S2022.pdf)
 # 
 
 # In[ ]:
