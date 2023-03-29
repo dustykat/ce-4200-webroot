ee0cda539fedc9c0fe8d44542c640c76ddc1e030
theodore cleveland <ted.cleveland@gmail.com>
2023-03-29 16:30:16 +0000

    add exercises

---
+++ ce-4200-notes/_build/jupyter_execute/lessons/qbs/qbs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Diversity and Inclusion
+# 
+# **Under development** 
+# 
+# Use link below to older lecture notes
+# 
+
+# ## References
+# 
+# 1. [Lawson (2021), *Diversity and Inclusion* Lecture to accompany CE 4200, Whitacre College of Engineering, Texas Tech University](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/LECTURE-SOURCE/LECTURE13_DIVERSITY_INCLUSION_F2021.pdf)
+# 
+
+# In[ ]:
+
+
+
+
