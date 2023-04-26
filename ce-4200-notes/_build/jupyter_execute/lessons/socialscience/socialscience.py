6797fe342458adbb428ada0eadf04420d08ef3b0
theodore cleveland <ted.cleveland@gmail.com>
2023-04-26 16:57:30 +0000

    update links

--- ce-4200-notes/_build/jupyter_execute/lessons/socialscience/socialscience.py
+++ ce-4200-notes/_build/jupyter_execute/lessons/socialscience/socialscience.py
@@ -10,7 +10,7 @@
 
 # ## References
 # 
-# 1. [Lawson (2022), *Social Science and Engineering* Lecture to accompany CE 4200, Whitacre College of Engineering, Texas Tech University](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/lessons/criticalthinking/LECTURE21_CriticalThinkingINTRODUCTION-S2022.pdf)
+# 1. [Lawson (2022), *Social Science and Engineering* Lecture to accompany CE 4200, Whitacre College of Engineering, Texas Tech University](http://54.243.252.9/ce-4200-webroot/ce-4200-notes/lessons/socialscience/LECTURE25_SocialScienceandCivilEngineeringINTRODUCTION-f21.pdf)
 # 
 
 # In[ ]:
