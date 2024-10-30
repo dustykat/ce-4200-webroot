c6f2143bc1ef95145371f5f97c91776468be94f1
theodore cleveland <ted.cleveland@gmail.com>
2024-10-30 23:54:16 +0000

    add net abet inserts

---
+++ ce-4200-notes/_build/jupyter_execute/lessons/cultural/cultural.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Evaluation of Cultural Considerations 
+# 
+# **This topic is an ABET required topic transferred from CE 4330 to this course**
+# 
+# 
+
+# ## Why Cultural Considerations Matter in Engineering
+# 
+# 
+# :::{admonition} Discussion 
+# "Can anyone think of a recent engineering project that had a direct impact on public health?"
+# :::
+
+# ## Key Cultural Considerations Considerations in Design 
+# 
+# 
+
+# ## Case Study: sumthin'
+# 
+# 
+# ### Key engineering failures included:
+# 
+# 
+# 
+# ### Engineering Design Lessons and Reforms
+# 
+# 
+# 
+# ### Conclusion
+# 
+# 
+
+# ## Practical Steps for Students 
+# 
+# ### Learn and Research
+# 
+# 
+# ### Collaborate
+# 
+# ### Design Thinking
+# 
+# ### Advocate: 
+# 
+# 
+# ## Conclusion 
+# 
+# :::{admonition} Include documented consideration of cultural considerations in your designs
+# "As you progress in your engineering careers, always remember: the designs you create can lead to ..."
+# :::
+
+# ## References
+# 
+# 1. Placeholder
+# 
+# 6. OpenAI (2024). Prompt: "Can you help me prepare a short script (for 40 minute presentation) on "Evaluation of Public Health Considerations" in the context of engineering design? The audience is engineering students about 4 years away from license eligibility?".  ChatGPT-4.0.  URL `https://chatgpt.com/c/66e1f0a5-21cc-800d-8b5a-ee9d9e8d2835`  
+# 
+# :::{note}
+# "The OpenAI URL provided in references helps retrieve specific content shared, but information is not publicly recoverable unless required by legal obligations."
+# :::
+
+# 
